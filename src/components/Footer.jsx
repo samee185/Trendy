23c567f891de2c6bf966b0dcb8b47e7d35b96924
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-10-15 01:34:42 +0100

    This code defines a Footer component for a website. It uses a responsive layout with three sections: logo and description, company links, and contact information.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      {/* Main footer content */}
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* Logo and description */}
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Repellendus, optios
+          </p>
+        </div>
+        {/* Company links */}
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        {/* Contact information */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>contact@foreveryou.com</li>
+          </ul>
+        </div>
+      </div>
+      {/* Copyright notice */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          copyright 2024@ forever.com -All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
+
