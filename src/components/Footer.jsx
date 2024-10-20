4d08da941315979fa1c106fb088a55766f92b1d5
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-10-20 13:16:25 +0100

    adding style to the logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import logo from "../assets/logo2.png";
 
 const Footer = () => {
   return (
@@ -8,7 +9,8 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* Logo and description */}
         <div>
-          <img src={assets.logo} className="mb-5 w-32" alt="" />
+        <img src={logo} className="w-10 mb-5 " alt="Logo" />
+          {/* <img src={assets.logo} className="mb-5 w-32" alt="" /> */}
           <p className="w-full md:w-2/3 text-gray-600">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
             Repellendus, optios
