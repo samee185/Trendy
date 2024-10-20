3a87de55efe4dd836bad31674efd20693c58540f
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-10-20 13:15:15 +0100

    all about the company

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const About = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+            impedit voluptates, consequatur temporibus amet nemo eum minus
+            voluptate earum hic.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            nihil labore, iusto obcaecati asperiores praesentium?
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+            harum.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
 
-export default About
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600" >
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standard
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Conveniece:</b>
+          <p className="text-gray-600">
+           With our user-friendly interface and hassle-free ordering process, shoppig has never been easier.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Services:</b>
+          <p className="text-gray-600">
+            Our team of dedicated professioals is here to assist you the way, ensuring your satisfaction is our top prioority.
+          </p>
+        </div>
+      </div>
+
+      <NewsLetterBox/>
+
+    </div>
+  );
+};
+
+export default About;
