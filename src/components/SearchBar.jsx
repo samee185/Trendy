be0119671b1b64c12edaec64c97ac1dcf2704869
samee <samueliy185@gmail.com>
2024-11-06 06:36:17 -0800

    updates

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -20,8 +20,8 @@ const SearchBar = () => {
 
   // Render search bar only if showSearch is true and on the collection page
   return showSearch && visible ? (
-    <div className="border-t border-b bg-gray-50 text-center">
-      <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+    <div className="border-t border-b bg-gray-200 text-center">
+      <div className="inline-flex items-center justify-center border border-gray-800 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
